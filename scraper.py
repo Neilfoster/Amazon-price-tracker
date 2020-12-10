e10e53ff8dcbb73ed6e3faaef68f9d265a47c26f
Neil Foster <neilfoster81@gmail.com>
2020-12-10 15:19:01 +0000

    Added imports and URL

---
+++ scraper.py
@@ -0,0 +1,8 @@
+import requests
+import bs4 import BeautifulSoup
+
+URL = 'https://www.amazon.co.uk/Nike-Unisex-Adults-Essential-Running/dp/B07ZTTQXXT/ref=sr_1_1?crid=3SHAZ3MMSL57Q&dchild=1&keywords=nike+air+max+95+mens+black&qid=1607613393&quartzVehicle=99-1304&replacementKeywords=nike+air+max+mens+black&sprefix=nike+air+max+95+mens+%2Caps%2C153&sr=8-1'
+
+
+def getPrice():
+    pass
